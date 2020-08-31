a783aefc0357f8d198b47fb1b76b0fee58bfac07
Geliostigma <zharikovjura@gmail.com>
2020-08-31 18:50:37 +0300

    added a frame to lesson 6

---
+++ nordic-it-java-q4/06/HomeWork/HomeWork5/src/com/example/PaintForYou.java
@@ -0,0 +1,126 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class PaintForYou {
+
+	public static void main(String[] args) {
+
+		System.out.println("Программа рисует рамку вокруг текста.");
+
+		var scanner = new Scanner(System.in);
+
+		System.out.println("Введите длину");
+		var lenght = scanner.nextInt();
+		var countLen = lenght;
+		var halfLen = (lenght / 2);
+
+		System.out.println("Введите ширину");
+		var width = scanner.nextInt();
+
+		System.out.println("Введите текст");
+		var text = scanner.next();
+		var textLen = text.length();
+		var indent = (width - textLen) / 2;
+
+		if (lenght > 2 && width > 2 && textLen < width - 1) {
+
+			for (int i = width; i > 0; i--) {
+				System.out.print("*");
+
+			}
+
+			System.out.println();
+			countLen--;
+
+			while (countLen != 0 && halfLen + 1 != countLen) {
+
+				System.out.print("*");
+
+				for (int i = width - 2; i > 0; i--) {
+					System.out.print(" ");
+				}
+
+				System.out.print("*");
+				System.out.println();
+				countLen--;
+
+			}
+
+			System.out.print("*");
+
+			if (width % 2 == 0) {
+
+				for (int i = indent - 1; i > 0; i--) {
+					System.out.print(" ");
+				}
+
+			} else {
+
+				for (int i = indent - 1; i > 0; i--) {
+					System.out.print(" ");
+
+				}
+
+			}
+
+			System.out.print(text);
+
+			if (width % 2 == 0 && textLen % 2 == 0) {
+
+				for (int i = indent - 1; i > 0; i--) {
+					System.out.print(" ");
+				}
+				
+			} else if (width % 2 == 0 && textLen % 2 != 0) {
+
+				for (int i = indent; i > 0; i--) {
+					System.out.print(" ");
+				}
+
+			} else if (width % 2 != 0 && textLen % 2 == 0) {
+
+				for (int i = indent; i > 0; i--) {
+					System.out.print(" ");
+				}
+
+			} else {
+
+				for (int i = indent - 1; i > 0; i--) {
+					System.out.print(" ");
+
+				}
+
+			}
+
+			System.out.print("*");
+			System.out.println();
+			countLen--;
+
+			while (countLen > 1) {
+
+				System.out.print("*");
+
+				for (int i = width - 2; i > 0; i--) {
+					System.out.print(" ");
+				}
+
+				System.out.print("*");
+				System.out.println();
+				countLen--;
+
+			}
+
+			for (int i = width; i > 0; i--) {
+				System.out.print("*");
+
+			}
+			
+			System.out.println();
+
+		} else {
+			System.out.println("Ошибка.");
+		}
+	}
+
+}
