c1e5a35b49ca978314fc2cbf24ff55c04a9c0784
Geliostigma <zharikovjura@gmail.com>
2020-09-07 22:37:49 +0300

    Added homework, lesson 10

---
+++ nordic-it-java-q4/10/HomeWork/OOP/src/com/zoo/animal/Leopard.java
@@ -0,0 +1,76 @@
+package com.zoo.animal;
+
+public class Leopard {
+
+	protected String name;
+	protected int age;
+	protected String color;
+	protected boolean sexMale;
+
+	public Leopard() {
+		
+	}
+
+	public Leopard(String name, int age, String color, boolean sexMale) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.color = color;
+		this.sexMale = sexMale;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public boolean isSexMale() {
+		return sexMale;
+	}
+
+	public void setSexMale(boolean sexMale) {
+		this.sexMale = sexMale;
+	}
+
+	public void say() {
+		System.out.println("Рравр");
+	}
+
+	public void eat(int food) {
+		--food;
+	}
+
+	public void mate() {
+
+	}
+	
+	public void hunt() {
+		System.out.println("Бежит за добычей");
+		
+	}
+
+	public void kill() {
+		System.out.println("Убивает добычу");
+		
+	}
+
+}
